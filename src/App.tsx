889ea09dd1c69a2da928bfc7625f450a970fd57c
tkdrb12 <tkdrb15@gmail.com>
2024-06-17 17:04:46 +0900

    chore: Remove unnecessary comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Box, Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { useEffect } from 'react';
 import styled from 'styled-components';
@@ -16,36 +15,7 @@ const App = () => {
     <div>
       <CanvasBox>
         <Canvas>
-          {' '}
-          <Box />
-          <Sky />
-          {/* <CameraControls />
-          <KeyboardControls
-            map={[
-              { name: 'forward', keys: ['ArrowUp', 'w', 'W'] },
-              { name: 'backward', keys: ['ArrowDown', 's', 'S'] },
-              { name: 'left', keys: ['ArrowLeft', 'a', 'A'] },
-              { name: 'right', keys: ['ArrowRight', 'd', 'D'] },
-              { name: 'jump', keys: ['Space'] },
-            ]}
-          >
-            <OrbitControls />
-            <ambientLight intensity={Math.PI / 2} />
-            <spotLight
-              position={[10, 10, 10]}
-              angle={0.15}
-              penumbra={1}
-              decay={0}
-              intensity={Math.PI}
-            />
-            <pointLight
-              position={[-10, 100, -10]}
-              decay={0}
-              intensity={Math.PI}
-            />
-            <Sky />
-            <Suspense fallback={null}></Suspense> */}
-          {/* </KeyboardControls> */}
+          <></>
         </Canvas>
       </CanvasBox>
     </div>
