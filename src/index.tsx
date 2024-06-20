0e3b5cbb74f7492b1d4472fe13e2ed550ae949f0
tkdrb12 <tkdrb15@gmail.com>
2024-06-20 15:27:34 +0900

    design: Add default style

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import { createRoot } from 'react-dom/client';
+import React from 'react';
+import './index.css';
 import App from './App';
 
 async function enableMocking() {
@@ -11,5 +13,10 @@ async function enableMocking() {
 
 enableMocking().then(() => {
   const root = createRoot(document.getElementById('root')!);
-  root.render(<App />);
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 });
